95c5b19bd524c4caff3dab085b23aac62e4afba4
Jamie Kowalczik <jamie.kowalczik@gmail.com>
2016-08-23 17:11:46 -0400

    commit

---
+++ library/ntc_file_copy.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Jason Edelman <jason@networktocode.com>
+# Network to Code, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: ntc_file_copy
+short_description: Copy a file to a remote network device over SCP.
+description:
+    - Copy a file to the flash (or bootflash) remote network device on supported platforms over SCP.
+    - Supported platforms include Cisco Nexus switches with NX-API, Cisco IOS switches or routers, Arista switches with eAPI.
+notes:
+    - On NXOS, the feature must be enabled with feature scp-server.
+    - On IOS and Arista EOS, the user must be at privelege 15.
+    - If the file is already present (md5 sums match), no transfer will take place.
+    - Check mode will tell you if the file would be copied.
+    - The same user credentials are used on the API/SSH channel and the SCP file transfer channel.
+author: Jason Edelman (@jedelman8)
+version_added: 1.9.2
+requirements:
+    - pyntc
+options:
+    platform:
+        description:
+            - Switch platform
+        required: true
+        choices: ['cisco_nxos_nxapi', 'arista_eos_eapi', 'cisco_ios']
+    local_file:
+        description:
+            - Path to local file. Local directory must exist.
+        required: true
+    remote_file:
+        description:
+            - Remote file path of the copy. Remote directories must exist.
+              If omitted, the name of the local file will be used.
+        required: false
+        default: null
+    file_system:
+        description:
+            - The remote file system of the device. If omitted,
+              devices that support a file_system parameter will use their default values.
+        required: false
+        default: null
+    host:
+        description:
+            - Hostame or IP address of switch.
+        required: true
+    username:
+        description:
+            - Username used to login to the target device
+        required: true
+    password:
+        description:
+            - Password used to login to the target device
+        required: true
+    secret:
+        description:
+            - Enable secret for devices connecting over SSH.
+        required: false
+    transport:
+        description:
+            - Transport protocol for API-based devices. Not used for actual file transfer.
+        required: false
+        default: null
+        choices: ['http', 'https']
+    port:
+        description:
+            - TCP/UDP port to connect to target device. If omitted standard port numbers will be used.
+              80 for HTTP; 443 for HTTPS; 22 for SSH.
+        required: false
+        default: null
+    ntc_host:
+        description:
+            - The name of a host as specified in an NTC configuration file.
+        required: false
+        default: null
+    ntc_conf_file:
+        description:
+            - The path to a local NTC configuration file. If omitted, and ntc_host is specified,
+              the system will look for a file given by the path in the environment variable PYNTC_CONF,
+              and then in the users home directory for a file called .ntc.conf.
+        required: false
+        default: null
+'''
+
+EXAMPLES = '''
+- ntc_file_copy:
+    platform: cisco_nxos_nxapi
+    local_file: /path/to/file
+    host: "{{ inventory_hostname }}"
+    username: "{{ username }}"
+    password: "{{ password }}"
+    transport: http
+- ntc_file_copy:
+    ntc_host: n9k1
+    ntc_conf_file: .ntc.conf
+    local_file: /path/to/file
+- ntc_file_copy:
+    ntc_host: eos_leaf
+    local_file: /path/to/file
+- ntc_file_copy:
+    platform: arista_eos_eapi
+    local_file: /path/to/file
+    remote_file: /path/to/remote_file
+    host: "{{ inventory_hostname }}"
+    username: "{{ username }}"
+    password: "{{ password }}"
+- ntc_file_copy:
+    platform: cisco_ios
+    local_file: "{{ local_file_1 }}"
+    host: "{{ inventory_hostname }}"
+    username: "{{ username }}"
+    password: "{{ password }}"
+    secret: "{{ secret }}"
+'''
+
+RETURN = '''
+transfer_status:
+    description: Whether a file was transfered. "No Transfer" or "Sent".
+    returned: success
+    type: string
+    sample: 'Sent'
+local_file:
+    description: The path of the local file.
+    returned: success
+    type: string
+    sample: '/path/to/local/file'
+remote_file:
+    description: The path of the remote file.
+    returned: success
+    type: string
+    sample: '/path/to/remote/file'
+'''
+
+import os
+
+try:
+    HAS_PYNTC = True
+    from pyntc import ntc_device, ntc_device_by_name
+except ImportError:
+    HAS_PYNTC = False
+
+PLATFORM_NXAPI = 'cisco_nxos_nxapi'
+PLATFORM_IOS = 'cisco_ios_ssh'
+PLATFORM_EAPI = 'arista_eos_eapi'
+PLATFORM_JUNOS = 'juniper_junos_netconf'
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            platform=dict(choices=[PLATFORM_NXAPI, PLATFORM_IOS, PLATFORM_EAPI, PLATFORM_JUNOS],
+                          required=False),
+            host=dict(required=False),
+            username=dict(required=False, type='str'),
+            password=dict(required=False, type='str'),
+            secret=dict(required=False),
+            transport=dict(required=False, choices=['http', 'https']),
+            port=dict(required=False, type='int'),
+            ntc_host=dict(required=False),
+            ntc_conf_file=dict(required=False),
+            local_file=dict(required=True),
+            remote_file=dict(required=False),
+            file_system=dict(required=False),
+        ),
+        mutually_exclusive=[['host', 'ntc_host'],
+                            ['ntc_host', 'secret'],
+                            ['ntc_host', 'transport'],
+                            ['ntc_host', 'port'],
+                            ['ntc_conf_file', 'secret'],
+                            ['ntc_conf_file', 'transport'],
+                            ['ntc_conf_file', 'port'],
+                           ],
+        required_one_of=[['host', 'ntc_host']],
+        required_together=[['host', 'username', 'password', 'platform']],
+        supports_check_mode=True
+    )
+
+    if not HAS_PYNTC:
+        module.fail_json(msg='pyntc Python library not found.')
+
+    platform = module.params['platform']
+    host = module.params['host']
+    username = module.params['username']
+    password = module.params['password']
+
+    ntc_host = module.params['ntc_host']
+    ntc_conf_file = module.params['ntc_conf_file']
+
+    transport = module.params['transport']
+    port = module.params['port']
+    secret = module.params['secret']
+
+    if ntc_host is not None:
+        device = ntc_device_by_name(ntc_host, ntc_conf_file)
+    else:
+        kwargs = {}
+        if transport is not None:
+            kwargs['transport'] = transport
+        if port is not None:
+            kwargs['port'] = port
+        if secret is not None:
+            kwargs['secret'] = secret
+
+        device_type = platform
+        device = ntc_device(device_type, host, username, password, **kwargs)
+
+    local_file = module.params['local_file']
+    remote_file = module.params['remote_file']
+    file_system = module.params['file_system']
+
+    device.open()
+
+    changed = False
+    transfer_status = 'No Transfer'
+    file_exists = True
+
+    if not os.path.isfile(local_file):
+        module.fail_json(msg="Local file {} not found".format(local_file))
+
+    if file_system:
+        remote_exists = device.file_copy_remote_exists(local_file, remote_file, file_system=file_system)
+    else:
+        remote_exists = device.file_copy_remote_exists(local_file, remote_file)
+
+    if not remote_exists:
+        changed = True
+        file_exists = False
+
+    if not module.check_mode and not file_exists:
+        try:
+            if file_system:
+                device.file_copy(local_file, remote_file, file_system=file_system)
+            else:
+                device.file_copy(local_file, remote_file)
+
+            transfer_status = 'Sent'
+        except Exception as e:
+            module.fail_json(msg=str(e))
+
+    device.close()
+
+    if remote_file is None:
+        remote_file = os.path.basename(local_file)
+
+    module.exit_json(changed=changed, transfer_status=transfer_status, local_file=local_file, remote_file=remote_file, file_system=file_system)
+
+from ansible.module_utils.basic import *
+main()
---
+++ library/ntc_reboot.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Jason Edelman <jason@networktocode.com>
+# Network to Code, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: ntc_reboot
+short_description: Reboot a network device.
+description:
+    - Reboot a network device, optionally on a timer.
+    - Supported platforms include Cisco Nexus switches with NX-API, Cisco IOS switches or routers, Arista switches with eAPI.
+Notes:
+    - The timer is only supported for IOS devices.
+author: Jason Edelman (@jedelman8)
+version_added: 1.9.2
+requirements:
+    - pyntc
+options:
+    platform:
+        description:
+            - Switch platform
+        required: true
+        choices: ['cisco_nxos_nxapi', 'arista_eos_eapi', 'cisco_ios']
+    timer:
+        description:
+            - Time in minutes after which the device will be rebooted.
+        required: false
+        default: null
+    confirm:
+        description:
+            - Safeguard boolean. Set to true if you're sure you want to reboot.
+        required: false
+        default: false
+    host:
+        description:
+            - Hostame or IP address of switch.
+        required: true
+    username:
+        description:
+            - Username used to login to the target device
+        required: true
+    password:
+        description:
+            - Password used to login to the target device
+        required: true
+    secret:
+        description:
+            - Enable secret for devices connecting over SSH.
+        required: false
+    transport:
+        description:
+            - Transport protocol for API-based devices.
+        required: false
+        default: https
+        choices: ['http', 'https']
+    port:
+        description:
+            - TCP/UDP port to connect to target device. If omitted standard port numbers will be used.
+              80 for HTTP; 443 for HTTPS; 22 for SSH.
+        required: false
+        default: null
+    ntc_host:
+        description:
+            - The name of a host as specified in an NTC configuration file.
+        required: false
+        default: null
+    ntc_conf_file:
+        description:
+            - The path to a local NTC configuration file. If omitted, and ntc_host is specified,
+              the system will look for a file given by the path in the environment variable PYNTC_CONF,
+              and then in the users home directory for a file called .ntc.conf.
+        required: false
+        default: null
+'''
+
+EXAMPLES = '''
+- ntc_reboot:
+    platform: cisco_nxos_nxapi
+    confirm: true
+    host: "{{ inventory_hostname }}"
+    username: "{{ username }}"
+    password: "{{ password }}"
+    transport: http
+
+- ntc_reboot:
+    ntc_host: n9k1
+    ntc_conf_file: .ntc.conf
+    confirm: true
+
+- ntc_file_copy:
+    platform: arista_eos_eapi
+    confirm: true
+    host: "{{ inventory_hostname }}"
+    username: "{{ username }}"
+    password: "{{ password }}"
+
+- ntc_file_copy:
+    platform: cisco_ios
+    confirm: true
+    timer: 5
+    host: "{{ inventory_hostname }}"
+    username: "{{ username }}"
+    password: "{{ password }}"
+    secret: "{{ secret }}"
+'''
+
+RETURN = '''
+rebooted:
+    description: Whether the device was instructed to reboot.
+    returned: success
+    type: boolean
+    sample: true
+'''
+
+try:
+    HAS_PYNTC = True
+    from pyntc import ntc_device, ntc_device_by_name
+except ImportError:
+    HAS_PYNTC = False
+
+PLATFORM_NXAPI = 'cisco_nxos_nxapi'
+PLATFORM_IOS = 'cisco_ios_ssh'
+PLATFORM_EAPI = 'arista_eos_eapi'
+PLATFORM_JUNOS = 'juniper_junos_netconf'
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            platform=dict(choices=[PLATFORM_NXAPI, PLATFORM_IOS, PLATFORM_EAPI, PLATFORM_JUNOS],
+                          required=False),
+            host=dict(required=False),
+            username=dict(required=False, type='str'),
+            password=dict(required=False, type='str'),
+            secret=dict(required=False),
+            transport=dict(required=False, choices=['http', 'https']),
+            port=dict(required=False, type='int'),
+            ntc_host=dict(required=False),
+            ntc_conf_file=dict(required=False),
+            confirm=dict(required=False, default=False, type='bool', choices=BOOLEANS),
+            timer=dict(requred=False, type='int'),
+        ),
+        mutually_exclusive=[['host', 'ntc_host'],
+                            ['ntc_host', 'secret'],
+                            ['ntc_host', 'transport'],
+                            ['ntc_host', 'port'],
+                            ['ntc_conf_file', 'secret'],
+                            ['ntc_conf_file', 'transport'],
+                            ['ntc_conf_file', 'port'],
+                           ],
+        required_one_of=[['host', 'ntc_host']],
+        required_together=[['host', 'username', 'password', 'platform']],
+        supports_check_mode=False
+    )
+
+    if not HAS_PYNTC:
+        module.fail_json(msg='pyntc Python library not found.')
+
+    platform = module.params['platform']
+    host = module.params['host']
+    username = module.params['username']
+    password = module.params['password']
+
+    ntc_host = module.params['ntc_host']
+    ntc_conf_file = module.params['ntc_conf_file']
+
+    transport = module.params['transport']
+    port = module.params['port']
+    secret = module.params['secret']
+
+    if ntc_host is not None:
+        device = ntc_device_by_name(ntc_host, ntc_conf_file)
+    else:
+        kwargs = {}
+        if transport is not None:
+            kwargs['transport'] = transport
+        if port is not None:
+            kwargs['port'] = port
+        if secret is not None:
+            kwargs['secret'] = secret
+
+        device_type = platform
+        device = ntc_device(device_type, host, username, password, **kwargs)
+
+    confirm = module.params['confirm']
+    timer = module.params['timer']
+
+    if not confirm:
+        module.fail_json(msg='confirm must be set to true for this module to work.')
+
+    supported_timer_platforms = [PLATFORM_IOS, PLATFORM_JUNOS]
+
+    if timer is not None \
+            and device.device_type not in supported_timer_platforms:
+        module.fail_json(msg='Timer parameter not supported on platform %s.' % platform)
+
+    device.open()
+
+    changed = False
+    rebooted = False
+
+    if timer is not None:
+        device.reboot(confirm=True, timer=timer)
+    else:
+        device.reboot(confirm=True)
+
+    changed = True
+    rebooted = True
+
+    device.close()
+    module.exit_json(changed=changed, rebooted=rebooted)
+
+from ansible.module_utils.basic import *
+main()
